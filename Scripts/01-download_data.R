aed070573311e7284f7e9c0b8816e49a932424b6
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-09-24 19:34:37 -0400

    Updated script to select relevant spreadsheets and filter by columns of interest/topic. And saved these relevant data sets.

--- Scripts/01-download_data.R
+++ Scripts/01-download_data.R
@@ -15,14 +15,82 @@ library(arrow)
 
 #### Download data ####
 # Code referenced from: https://stackoverflow.com/questions/7151145/unzip-a-tar-gz-file & https://www.youtube.com/watch?v=LKprlFCLnSA&ab_channel=RiffomonasProject
-
 options(timeout = 400)
 hansard_data <- "https://www.lipad.ca/media/lipadcsv-1.1.0.tar.bz2"
 download.file(hansard_data, destfile = "lipadcsv-1.1.0.tar.bz2")
 
-# View and selected individual files of interest # 
+# Select individual files of interest, select relevant columns, and filter by subtopic # 
 # Code referenced from: https://arrow.apache.org/docs/r/reference/read_delim_arrow.html
 
 archive("lipadcsv-1.1.0.tar.bz2")
 
-read_tsv_arrow(archive_read("lipadcsv-1.1.0.tar.bz2", "lipad/2018/1/2018-1-29.csv"))
+# Bill C-65 First Reading, November 7, 2017 # 
+raw_nov_7 <- read_delim_arrow(archive_read("lipadcsv-1.1.0.tar.bz2", "lipad/2017/11/2017-11-7.csv")) |>
+  select(speechdate,
+         speakeroldname,
+         maintopic,
+         subtopic,
+         speechtext,
+         speakerparty,
+         speakerriding,
+         speakername) |>
+  filter(subtopic == "Canada Labour Code")
+
+# Bill C-65 Second Reading, January 29, 2018 #  
+raw_jan_29 <- read_delim_arrow(archive_read("lipadcsv-1.1.0.tar.bz2", "lipad/2018/1/2018-1-29.csv")) |>
+  select(speechdate,
+         speakeroldname,
+         maintopic,
+         subtopic,
+         speechtext,
+         speakerparty,
+         speakerriding,
+         speakername) |>
+  filter(subtopic == "Canada Labour Code")
+
+# Bill C-65 End of Report Stage & Third Reading, May 7, 2018 #  
+raw_may_7 <- read_delim_arrow(archive_read("lipadcsv-1.1.0.tar.bz2", "lipad/2018/5/2018-5-7.csv")) |>
+  select(speechdate,
+         speakeroldname,
+         maintopic,
+         subtopic,
+         speechtext,
+         speakerparty,
+         speakerriding,
+         speakername) |>
+  filter(subtopic == "Canada Labour Code")
+
+# Bill C-65 Consideration of Senate amendments, Oct 16, 2018 #  
+raw_oct_16 <- read_delim_arrow(archive_read("lipadcsv-1.1.0.tar.bz2", "lipad/2018/10/2018-10-16.csv")) |>
+  select(speechdate,
+         speakeroldname,
+         maintopic,
+         subtopic,
+         speechtext,
+         speakerparty,
+         speakerriding,
+         speakername) |>
+  filter(subtopic == "Canada Labour Code")
+
+# Bill C-65 Consideration of Senate amendments, Oct 17, 2018 #  
+raw_oct_17 <- read_delim_arrow(archive_read("lipadcsv-1.1.0.tar.bz2", "lipad/2018/10/2018-10-17.csv")) |>
+  select(speechdate,
+         speakeroldname,
+         maintopic,
+         subtopic,
+         speechtext,
+         speakerparty,
+         speakerriding,
+         speakername) |>
+  filter(subtopic == "Canada Labour Code")
+
+#### Save data sets ####
+write_csv(x = raw_nov_7, file = "Inputs/Data/raw_nov_7.csv")
+
+write_csv(x = raw_jan_29, file = "Inputs/Data/raw_jan_29.csv")
+
+write_csv(x = raw_may_7, file = "Inputs/Data/raw_may_7.csv")
+
+write_csv(x = raw_oct_16, file = "Inputs/Data/raw_oct_16.csv")
+
+write_csv(x = raw_oct_17, file = "Inputs/Data/raw_oct_17.csv")
