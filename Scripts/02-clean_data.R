f652cb6c5584f9b06f5b984015cd750bbeebdfbd
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-12-03 20:06:05 -0500

    data cleaning

--- Scripts/02-clean_data.R
+++ Scripts/02-clean_data.R
@@ -4,7 +4,8 @@
 # Date: 3 December 2023 
 # Contact: inessa.deangelis@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 01-download_data.R
+# Pre-requisites: 
+  # 01-download_data.R
 
 #### Workspace setup ####
 library(tidyverse)
